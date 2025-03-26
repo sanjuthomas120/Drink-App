0d2848ac05d57a20385174733081af4ec37a7387
sanju <sanjuthom120@gmail.com>
2025-03-26 23:42:05 +0530

    some fix

--- backend/index.js
+++ backend/index.js
@@ -2,32 +2,27 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
-const path = require('path')
+const path = require('path');
 const MenuRoutes = require('./routes/MenuRoutes');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname,'../frontend/build')))
-
-app.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'))
-})
 
 app.use('/api', MenuRoutes);
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => {
-  console.log('Connected to MongoDB');
-})
-.catch((error) => { 
-  console.log('Error connecting to MongoDB:', error.message);
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 
-
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch((error) => console.log('Error connecting to MongoDB:', error.message));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
