91dc6334b956ce3ac4a9b861c0cf1bac6b92afde
sanju <sanjuthom120@gmail.com>
2025-03-27 08:47:07 +0530

    port error

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://drink-app-w7lr.onrender.com/api", 
+    baseURL: "http://drink-app-w7lr.onrender.com:10000/api", 
     headers: {
         'Content-Type': 'application/json',
     },
